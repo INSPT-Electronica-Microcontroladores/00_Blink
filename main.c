35ee1a317ad2fd9bd350611f0e7236b4b61e8086
Marcelo Vieytes <mvieytes@gmail.com>
2025-11-07 20:11:27 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+/*
+ * Utiliza el GPIO22 para conectar el LED,
+ * de modo que se puede usar como target
+ * cualquier modelo de Raspberry Pi Pico
+ */
+#define LED_PIN  (22)
+
+int main() {
+    stdio_init_all();
+
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    while (true) {
+        gpio_put(LED_PIN, 1);
+        sleep_ms(500);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(500);
+        printf("Parpadeo LED!\n");
+    }
+}
